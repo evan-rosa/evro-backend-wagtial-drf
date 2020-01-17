1b7915257ff65ddca8f61a8d02e95b88bbddc29b
evr01 <erosa26@gmail.com>
2020-01-17 17:46:03 -0500

    added stage url to allowed hosts

--- evro/settings/base.py
+++ evro/settings/base.py
@@ -150,6 +150,10 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
