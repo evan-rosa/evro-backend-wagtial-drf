74cdae9569954a12624c544ce69b677486c1ccb8
evr01 <erosa26@gmail.com>
2020-01-17 18:24:25 -0500

    backend staticfiles

--- evro/settings/base.py
+++ evro/settings/base.py
@@ -156,7 +156,7 @@ STATICFILES_DIRS = [
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/staticfiles/static/'
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
